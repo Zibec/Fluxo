b47ea8bfd3a35445ca6f28083e9a097eb6b8ee31
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2025-08-14 20:56:55 -0300

    feat: inicialização do front-end e back-end

---
+++ backend/src/test/java/com/fluxo/fluxoapp/FluxoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxo.fluxoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
