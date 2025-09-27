3e4ff09381352908fcd81ed0bcd8b4574b08650f
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-09-27 16:46:57 -0300

    fix

--- backend/mainDomain/src/main/java/orcamento/OrcamentoService.java
+++ backend/mainDomain/src/main/java/orcamento/OrcamentoService.java
@@ -12,19 +12,19 @@ public class OrcamentoService {
         this.transacaoSumario = transacaoSumario;
     }
 
-    public void CriarOrcamentoMensal(String usuarioId, String categoriaid, YearMonth anoMes, BigDecimal limite){
+    public void criarOrcamentoMensal(String usuarioId, String categoriaid, YearMonth anoMes, BigDecimal limite){
 
         var chave = new OrcamentoChave(usuarioId, anoMes, categoriaid);
         orcamentoRepositorio.salvarNovo(chave, new Orcamento(limite));
     }
 
-    public void AtualizarOrcamentoMensal(String usuarioId, String categoriaid, YearMonth anoMes, BigDecimal limite){
+    public void atualizarOrcamentoMensal(String usuarioId, String categoriaid, YearMonth anoMes, BigDecimal limite){
 
         var chave = new OrcamentoChave(usuarioId, anoMes, categoriaid);
         orcamentoRepositorio.atualizarOrcamento(chave, new Orcamento(limite));
     }
 
-    public BigDecimal SaldoMensalTotal(String usuarioId, String categoriaid, YearMonth anoMes){
+    public BigDecimal saldoMensalTotal(String usuarioId, String categoriaid, YearMonth anoMes){
         var chave = new OrcamentoChave(usuarioId, anoMes, categoriaid);
         var orcamento = orcamentoRepositorio.obterOrcamento(chave)
                 .orElseThrow(()-> new IllegalStateException("Não existe um orçamento para essa chave"));
