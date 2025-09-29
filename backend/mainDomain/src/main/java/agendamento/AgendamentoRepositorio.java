8964dcfd8db4328b893fc939645280e51d38cb27
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-09-29 13:40:21 -0300

    test

--- backend/mainDomain/src/main/java/agendamento/AgendamentoRepositorio.java
+++ backend/mainDomain/src/main/java/agendamento/AgendamentoRepositorio.java
@@ -1,8 +1,20 @@
 package agendamento;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
-public interface AgendamentoRepositorio {
-    void salvar(Agendamento a);
-    Optional<Agendamento> obter(String id);
+
+public class AgendamentoRepositorio {
+
+    private final Map<String, Agendamento> agendamento = new ConcurrentHashMap<>();
+
+    /** Salva (insere/atualiza) */
+    public void salvar(Agendamento a) {
+        agendamento.put(a.getId(), a);
+    }
+
+    /** Busca por id */
+    public Optional<Agendamento> obter(String id) {
+        return Optional.ofNullable(agendamento.get(id));
+    }
 }
