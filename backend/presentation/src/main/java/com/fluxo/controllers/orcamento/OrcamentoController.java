1394f018d2deb84c0bc4812940733e2832ab9381
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-11-03 17:49:10 -0300

    orcamentoJpa, jpaRepository, repository Impl feitos para orcamento so falta controller e testes

--- backend/presentation/src/main/java/com/fluxo/controllers/orcamento/OrcamentoController.java
+++ backend/presentation/src/main/java/com/fluxo/controllers/orcamento/OrcamentoController.java
@@ -1,4 +1,15 @@
 package com.fluxo.controllers.orcamento;
 
+import conta.ContaService;
+import orcamento.OrcamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orcamento")
 public class OrcamentoController {
+
+    @Autowired
+    private OrcamentoService service;
 }
