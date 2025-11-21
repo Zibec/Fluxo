4f67203f30635d022d48550319375a2bc4d296b3
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2025-11-21 11:16:44 -0300

    feat: Modulo de Aplicação

--- backend/application/src/main/java/analise/RelatoriosService.java
+++ backend/application/src/main/java/analise/RelatoriosService.java
@@ -1,4 +1,36 @@
 package analise;
 
-public class RelatorioService {
+import cartao.CartaoService;
+import conta.ContaService;
+import generics.FormaPagamento;
+
+import java.math.BigDecimal;
+
+public class RelatoriosService {
+
+    private ContaService contaService;
+
+    private CartaoService cartaoService;
+
+    public RelatoriosService(ContaService contaService, CartaoService cartaoService) {
+        this.cartaoService = cartaoService;
+        this.contaService = contaService;
+    }
+
+    public BigDecimal calcularDinheiroTotal(String userId){
+        BigDecimal total = BigDecimal.ZERO;
+
+        BigDecimal totalContas = contaService.obterPorUsuarioId(userId).stream()
+                .map(FormaPagamento::getSaldo)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal totalCartoes = cartaoService.obterPorUsuarioId(userId).stream()
+                .map(FormaPagamento::getSaldo)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        total = total.add(totalContas);
+        total = total.add(totalCartoes);
+
+        return total;
+    }
 }
